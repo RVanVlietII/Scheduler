0b2355189e08f87313f55f7ff236c2f598554165
RVanVlietII <rcvanvlietii@gmail.com>
2023-07-04 12:56:24 -0400

    defined blockHour and descriptionEl to identify with specific row in event scheduler

--- assets/script.js
+++ assets/script.js
@@ -21,9 +21,9 @@ $(function () {
     localStorage.setItem("hour-" + hour, JSON.stringify(userInput));
   });
 
+  let blockHour = parseInt($(this)).attr("id").split("-")[1];
+  let descriptionEl = $(this).find('.description');
 
-  saveBtn.on('click', handleFormSubmit);
-  event.preventDefault();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
